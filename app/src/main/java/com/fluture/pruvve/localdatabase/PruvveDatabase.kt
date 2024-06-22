13f486b537357021ab30091844432762b6e09533
Pshypher <bshote@getcarbon.co>
2024-06-22 20:54:55 +0100

    Sets export schema flag to false

--- app/src/main/java/com/fluture/pruvve/localdatabase/PruvveDatabase.kt
+++ app/src/main/java/com/fluture/pruvve/localdatabase/PruvveDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [SavedPost::class], version = 2,  exportSchema = true)
+@Database(entities = [SavedPost::class], version = 1,  exportSchema = false)
 abstract class PruvveDatabase: RoomDatabase() {
     abstract val postDao: PostDao
 
